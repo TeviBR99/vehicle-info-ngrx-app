e9f1b20a71bc2b56a534212b62bb3285b0f75659
TeviBR99 <92033034+TeviBR99@users.noreply.github.com>
2025-08-10 12:31:21 +0200

    Search made

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,7 @@ export class AppComponent implements OnInit {
 
   public brands: Signal<Brand[]> = signal([]);
   public filteredBrands: Brand[] = [];
-  public searchBrand = signal('');
+  public searchBrand = '';
 
   constructor(private readonly vehicleService: VehicleService) {
     effect(() => {
@@ -30,19 +30,18 @@ export class AppComponent implements OnInit {
     if(this.brands()?.length === 0){
       this.vehicleService.getBrandsList();
       this.brands = this.vehicleService.brands
-      this.filteredBrands = this.brands();
+    }
+  }
+
+  public updateSearch(brandToSearch: string): void {
+    if(brandToSearch?.length > 0){
+      this.filteredBrands = this.brands().filter(b => b.name.toLowerCase().includes(brandToSearch));
     }
   }
 
   public goToBrandInfo(brand: Brand): void {
     console.log('Navigating to brand info for:', brand);
     // this.vehicleService.goToBrandInfo(brand);
-  }
-
-  public updateSearch(value: string): void {
-    this.searchBrand.set(value);
-    const brandToSearch = value.toLowerCase();
-    this.filteredBrands = this.brands().filter(b => b.name.toLowerCase().includes(brandToSearch));
   }
 
 }
