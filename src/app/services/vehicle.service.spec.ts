0781e68aba79f6807affa0bbd58e6264e671b762
TeviBR99 <92033034+TeviBR99@users.noreply.github.com>
2025-08-14 13:08:49 +0200

    Update vehicle.service.spec.ts

--- src/app/services/vehicle.service.spec.ts
+++ src/app/services/vehicle.service.spec.ts
@@ -1,12 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 
 import { VehicleService } from './vehicle.service';
+import { provideMockStore } from '@ngrx/store/testing';
+import { initialBrandState } from '../store/reducers';
 
 describe('VehicleService', () => {
   let service: VehicleService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        VehicleService,
+        provideMockStore({ initialState: initialBrandState }),
+      ]
+    });
     service = TestBed.inject(VehicleService);
   });
 
